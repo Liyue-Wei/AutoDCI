0fc343bf201cb5c258c8593cd8271526f83eb1b3
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-03-21 23:11:46 +0800

    Update DuanCI_CB.py
    新增if __name__ == "__main__"

--- DuanCI_Cloud_Based/DuanCI_CB.py
+++ DuanCI_Cloud_Based/DuanCI_CB.py
@@ -1,6 +1,7 @@
 from Extension_Modules import file_directory as fd
 import os
 import time
+import threading
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -17,16 +18,25 @@ driver.get("https://ckip.iis.sinica.edu.tw/service/ckiptagger/")
 
 element_strIN = driver.find_element(By.XPATH, "/html/body/textarea[4]")
 element_strOUT = driver.find_element(By.XPATH, "/html/body/textarea[5]")
-element_unSEL_1 = driver.find_element(By.XPATH, "/html/body/label[4]/input")
-element_unSEL_2 = driver.find_element(By.XPATH, "/html/body/label[5]/input")
+element_unSEL_1 = driver.find_element(By.XPATH, "/html/body/label[4]/input").click()
+element_unSEL_2 = driver.find_element(By.XPATH, "/html/body/label[5]/input").click()
 element_button = driver.find_element(By.XPATH, "/html/body/button")
 
-element_strIN.clear()
-element_unSEL_1.click()
-element_unSEL_2.click()
+def strIO(fileIN):
+    element_strIN.clear()
+    element_strOUT.clear()
 
-element_strIN.send_keys("餐廳評論分析系統 輸入一則關於餐廳的中文評論，系統會根據菜名及情緒字典等語彙庫偵測出評論對象和內容，再透過依存句法分析找出評論對象與內容的組合，判斷每一組評價內容的正負面。")
-element_button.click()
-time.sleep(1)
-TEXT = element_strOUT.get_attribute("value")
-print(TEXT)
+    element_strIN.send_keys(fileIN)
+    element_button.click()
+
+    fileOUT = element_strOUT.get_attribute("value")
+    while (fileOUT == ''):
+        fileOUT = element_strOUT.get_attribute("value")
+
+    print(fileOUT)
+
+def main():
+    strIO("這是一份測試文件")
+
+if __name__ == "__main__":
+    main()
